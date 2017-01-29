3f3d2993b524277dd7be538c3e9e64c78a53ea86
Pablo <psansberro@gmail.com>
2017-01-29 19:22:57 -0300

    Revert "Revert "Agregado de MVVM""
    This reverts commit b1ce886dcc95608b0bc986856bd3bfc8921d7562.

---
+++ Xamarin Git/Xamarin_Git/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xamarin_Git.ViewModel
+{
+    public class MainPageViewModel
+    {
+
+
+        public string Saludos
+        {
+            get { return "Hola gatos"; }
+        }
+    }
+    
+}
